85e36fb374e1172f74a1c29993e7592787d6f78e
gr98 <mr.guorui@163.com>
2018-04-13 10:47:38 +0800

    Add some files

---
+++ WebApplication1/WebApplication1/Controllers/TestController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class TestController : Controller
+    {
+        private TestDbContext db = new TestDbContext();
+
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View(db.Class1.ToList());
+        }
+
+        // GET: Test/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Class1 class1 = db.Class1.Find(id);
+            if (class1 == null)
+            {
+                return HttpNotFound();
+            }
+            return View(class1);
+        }
+
+        // GET: Test/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Test/Create
+        // 为了防止“过多发布”攻击，请启用要绑定到的特定属性，有关 
+        // 详细信息，请参阅 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Tid,Name")] Class1 class1)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Class1.Add(class1);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(class1);
+        }
+
+        // GET: Test/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Class1 class1 = db.Class1.Find(id);
+            if (class1 == null)
+            {
+                return HttpNotFound();
+            }
+            return View(class1);
+        }
+
+        // POST: Test/Edit/5
+        // 为了防止“过多发布”攻击，请启用要绑定到的特定属性，有关 
+        // 详细信息，请参阅 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Tid,Name")] Class1 class1)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(class1).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(class1);
+        }
+
+        // GET: Test/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Class1 class1 = db.Class1.Find(id);
+            if (class1 == null)
+            {
+                return HttpNotFound();
+            }
+            return View(class1);
+        }
+
+        // POST: Test/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Class1 class1 = db.Class1.Find(id);
+            db.Class1.Remove(class1);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Class1
+    {
+        [Key]
+        public virtual int Tid { get; set; }
+        public virtual string Name { get; set; }
+    }
+}
